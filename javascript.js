8755545dc3aae655fd1ce017a4c343cc02d6765c
ninerxsolution <ninerxsolution@gmail.com>
2022-11-28 10:58:31 +0700

    281122

--- javascript.js
+++ javascript.js
@@ -1,1 +1,34 @@
-alert("KUYKUYKUYKUY");
+var name = "GGWP";
+var number = 20;
+var array = [5, 3, 10];
+
+let nametwo = "mattha";
+let num_num = 50;
+let ara = [50, 20];
+
+var object = {
+  size: 10,
+  name: "dang",
+};
+var some_s = 50;
+console.log(some_s.toString() + 80);
+
+console.log(object.name, object.size);
+
+var totype = nametwo === number;
+console.log(totype);
+
+function funcName(para_1, para_2) {
+    return para_1 + para_2;
+}
+console.log(funcName(20,30));
+
+function funcName_2() {
+    var gimme = 5;
+    console.log();
+    return gimme;
+}
+funcName_2();
+// togimme = funcName_2();
+// gimme = 666;
+console.log(togimme);
